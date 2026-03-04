45d1e7999add599e4c5cc545b9ab2a200a47739b
am1099 <75802692+am1099@users.noreply.github.com>
2026-03-04 11:42:59 +0000

    Update server.js

--- server.js
+++ server.js
@@ -45,8 +45,17 @@ app.use(
   })
 );
 
-// Preflight always succeeds
-app.options("*", cors());
+app.use((req, res, next) => {
+  res.setHeader("Access-Control-Allow-Origin", "*");
+  res.setHeader("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  
+  if (req.method === "OPTIONS") {
+    return res.sendStatus(204);
+  }
+
+  next();
+});
 
 app.use(express.json());
 
