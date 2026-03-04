4e242aaf1a6ab7d1079fa3b950ca4bd9f5fd2857
am1099 <75802692+am1099@users.noreply.github.com>
2026-03-04 04:34:19 +0000

    Enhance CORS configuration for API endpoints

--- server.js
+++ server.js
@@ -18,11 +18,21 @@ const upload = multer({ dest: "/tmp" });
 
 app.use(cors({
   origin: "*",
-  methods: ["POST", "GET", "OPTIONS"],
-  allowedHeaders: ["Content-Type"]
+  methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
+  allowedHeaders: ["Content-Type", "Authorization"],
+  credentials: false
 }));
 
-app.options("*", cors());
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Methods", "GET,POST,PUT,DELETE,OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.options("*", (req, res) => {
+  res.status(200).send();
+});
 
 app.use(express.json());
 
